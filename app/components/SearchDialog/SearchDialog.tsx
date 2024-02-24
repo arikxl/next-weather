34c7b8498acac62358113d9318020114b82588fd
Arik Alexandrov <arikxl@gmail.com>
2024-02-24 02:00:25 +0200

    navbar

---
+++ app/components/SearchDialog/SearchDialog.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { commandIcon } from '@/app/utils/Icons'
+import { Button } from '@/components/ui/button'
+import { Dialog, DialogTrigger } from '@/components/ui/dialog'
+import React from 'react'
+
+const SearchDialog = () => {
+    return (
+        <div className='search-btn'>
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Button variant='outline'
+                        className='border inline-flex items-center justify-center text-sm font-medium hover:dark:bg-[#131313] hover:bg-slate-100  ease-in-out duration-200'>
+                        <p className='text-sm text-muted-foreground'>Search here...</p>
+                        <div className="command dark:bg-[#262626] bg-slate-200  py-[2px] pl-[5px] pr-[7px] rounded-sm ml-[10rem] flex items-center gap-2">
+                            {commandIcon}
+                            <span className='text-[9px]'>F</span>
+                        </div>
+                    </Button>
+                </DialogTrigger>
+            </Dialog>
+        </div>
+    )
+}
+
+export default SearchDialog
